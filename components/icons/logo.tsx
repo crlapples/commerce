7cb9157e82c91cc4e2ddcac367f5c93a480011d4
crlapples19 <crlapples19@gmail.com>
2025-05-26 05:25:48 +0000

    a

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 export default function LogoIcon(props: React.ComponentProps<'img'>) {
   return (
     <img
-      src="/favicon.jpg"
+      src="https://ingameshop.vercel.app/favicon.jpg"
       alt={`${process.env.SITE_NAME} logo`}
       {...props}
       className={clsx('h-4 w-4 rounded-sm', props.className)}
