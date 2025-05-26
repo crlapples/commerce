a4d3972942c09b4f9f528de1f5aa09865e52c66b
crlapples19 <crlapples19@gmail.com>
2025-05-26 19:42:19 +0000

    a

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,23 @@ import CarouselScrollWrapper from 'components/CarouselScrollWrapper';
 
 export const metadata = {
   description:
-    'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
+    'High-performance video game themed ecommerce store built with Next.js, Vercel, and PayPal.',
   openGraph: {
-    type: 'website'
-  }
+    type: 'website',
+    title: 'In-Game Shop',
+    description:
+      'Shop the best video game products with our high-performance ecommerce store built with Next.js, Vercel, and Shopify.',
+    url: 'https://ingameshop.vercel.app', // Replace with your website URL
+    images: [
+      {
+        url: 'https://ingameshop.vercel.app/ingameshop1.png', // Replace with the URL of your desired preview image
+        width: 1200, // Recommended width for OpenGraph images
+        height: 630, // Recommended height for OpenGraph images
+        alt: 'Ecommerce Store Preview Image',
+      },
+    ],
+    siteName: 'In-Game Shop',
+  },
 };
 
 export default function HomePage() {
