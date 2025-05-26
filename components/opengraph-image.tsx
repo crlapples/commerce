dd9e2fd72856dfea4b8393418944d122a3d99671
crlapples19 <crlapples19@gmail.com>
2025-05-26 19:49:28 +0000

    a

--- components/opengraph-image.tsx
+++ components/opengraph-image.tsx
@@ -24,7 +24,11 @@ export default async function OpengraphImage(
     (
       <div tw="flex h-full w-full flex-col items-center justify-center bg-black">
         <div tw="flex flex-none items-center justify-center border border-neutral-700 h-[160px] w-[160px] rounded-3xl">
-          <LogoIcon width={64} height={58} />
+          <img
+            src='/ingameshop1.png' // Replace with the URL of your desired preview image
+            width={1200} // Recommended width for OpenGraph images
+            height={630} // Recommended height for OpenGraph images
+            alt='Ecommerce Store Preview Image' />
         </div>
         <p tw="mt-12 text-6xl font-bold text-white">{title}</p>
       </div>
